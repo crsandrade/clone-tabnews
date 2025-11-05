38a19329a8a9f904b8989a2cc364d5d126ddc98b
Rafael Andrade <rafael_asc@outlook.com>
2025-11-05 18:49:08 -0300

    First blood

---
+++ README.md
@@ -0,0 +1,2 @@
+# clone-tabnews
+Projeto do curso.dev para formação de programadores capacitados.
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Venda todos seus wemix</h1>
+}
+
+export default Home;
